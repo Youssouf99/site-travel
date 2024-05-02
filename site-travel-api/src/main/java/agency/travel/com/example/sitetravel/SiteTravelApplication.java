fa5af6b8ab08ef7a070dc71ba8f10ae71d4a2ce9
Youssouf <youssouf.brahimazib@vinci-autoroutes.com>
2024-05-02 15:12:40 +0200

    initial project springboot

---
+++ site-travel-api/src/main/java/agency/travel/com/example/sitetravel/SiteTravelApplication.java
@@ -0,0 +1,13 @@
+package agency.travel.com.example.sitetravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteTravelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiteTravelApplication.class, args);
+    }
+
+}
---
+++ site-travel-api/src/test/java/agency/travel/com/example/sitetravel/SiteTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package agency.travel.com.example.sitetravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteTravelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
