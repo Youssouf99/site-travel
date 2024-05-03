1d2ac5a9c768786cfb9f0e1a0386ba83effa6fbe
Youssouf <youssouf.brahimazib@vinci-autoroutes.com>
2024-05-03 17:04:59 +0200

    feat(Mapper): add mappers user

--- site-travel-api/src/main/java/agency/travel/com/example/sitetravel/mappers/UserMapper.java
+++ site-travel-api/src/main/java/agency/travel/com/example/sitetravel/mappers/UserMapper.java
@@ -1,2 +1,25 @@
-package agency.travel.com.example.sitetravel.mappers;public interface UserMapper {
+package agency.travel.com.example.sitetravel.mappers;
+
+import agency.travel.com.example.sitetravel.dtos.UserDto;
+import agency.travel.com.example.sitetravel.entities.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    UserDto userToUserDto(User user);
+
+    List<UserDto> usersToUserDtos(List<User> users);
+
+    User userDtoToUser(UserDto userDto);
+
+
+    List<User> userDtosToUsers(List<UserDto> userDtos);
+
+
+
 }
